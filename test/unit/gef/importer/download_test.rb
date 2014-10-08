073eb52ae7548e1ec57162cb65485f647c335713
Miguel Torres <migtorres@gmail.com>
2014-10-08 18:17:55 +0100

    rubocop code improvements

--- test/unit/gef/importer/download_test.rb
+++ test/unit/gef/importer/download_test.rb
@@ -8,43 +8,44 @@ class DownloadTest < ActiveSupport::TestCase
   end
 
   test '#new creates an S3 connection' do
-    AWS::S3.expects(:new).with({
-      :access_key_id     => '123',
-      :secret_access_key => 'abc'
-    })
+    AWS::S3.expects(:new).with(
+      access_key_id: '123',
+      secret_access_key: 'abc'
+    )
 
-    Gef::Download.new()
+    Gef::Importer::Download.new
   end
 
-  test '.download_current_wdpa_to retrieves the latest WDPA from S3, and saves it to the
-   given filename' do
-    latest_file_mock = mock()
+  test '.download_current_wdpa_to retrieves the latest WDPA from S3,
+   and saves it to the given filename' do
+    latest_file_mock = mock
     latest_file_mock.stubs(:last_modified).returns(2.days.ago)
-    latest_file_mock.expects(:read).returns("")
+    latest_file_mock.expects(:read).returns('')
 
-    oldest_file_mock = mock()
+    oldest_file_mock = mock
     oldest_file_mock.stubs(:last_modified).returns(10.days.ago)
-    oldest_file_mock.stubs(:read).raises(Exception, "Expected the oldest file to not be downloaded")
+    oldest_file_mock.stubs(:read)
+      .raises(Exception, 'Expected the oldest file to not be downloaded')
 
-    bucket_mock = mock()
+    bucket_mock = mock
     bucket_mock.stubs(:objects).returns([
       latest_file_mock,
       oldest_file_mock
     ])
 
-    s3_mock = mock()
-    s3_mock.stubs(:buckets).returns({'wdpa' => bucket_mock})
+    s3_mock = mock
+    s3_mock.stubs(:buckets).returns('wdpa' => bucket_mock)
 
     AWS::S3.expects(:new).returns(s3_mock)
 
-    filename = 'hey_this_is_a_filename.zip'
+    filename = 'hey_this_is_a_filename.mdb'
 
-    file_write_mock = mock()
+    file_write_mock = mock
     file_write_mock.stubs(:write)
-    File.expects(:open).
-      with(filename, 'w:ASCII-8BIT').
-      yields(file_write_mock)
+    File.expects(:open)
+      .with(filename)
+      .yields(file_write_mock)
 
-    Wdpa::S3.download_current_wdpa_to filename: filename
+    Gef::Importer::Download.file filename: filename
   end
 end
