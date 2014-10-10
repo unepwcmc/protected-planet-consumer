a8b9d7a8cbe5d5d34b96b7deb8f6303f32280894
Miguel Torres <migtorres@gmail.com>
2014-10-10 16:56:53 +0100

    adds s3 module

--- lib/modules/s3.rb
+++ lib/modules/s3.rb
@@ -1,14 +1,29 @@
 class S3
-  def initialize
+  def initialize bucket_name
     @s3 = AWS::S3.new({
       access_key_id: Rails.application.secrets.aws_access_key_id,
       secret_access_key: Rails.application.secrets.aws_secret_access_key
     })
+    @bucket_name = bucket_name
   end
 
-  def download_from_bucket(bucket: bucket,filename: filename)
-
-
+  def download_from_bucket(filename: filename)
+    download_last_file_to(filename: filename)
   end
 
+  def download_last_file_to filename: filename
+    File.open(filename, 'w:ASCII-8BIT') do |file|
+      file.write last_file.read
+    end
+  end
+
+  private
+
+  def last_file
+    available_files.sort_by(&:last_modified).last
+  end
+
+  def available_files
+    @s3.buckets[@bucket_name].objects
+  end
 end
