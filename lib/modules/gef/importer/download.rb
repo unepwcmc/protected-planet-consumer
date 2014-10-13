59490c2b297105424684a465c108081b104d4228
Miguel Torres <migtorres@gmail.com>
2014-10-13 11:38:39 +0100

    adds gef importer download

---
+++ lib/modules/gef/importer/download.rb
@@ -0,0 +1,10 @@
+class Gef::Importer::Download
+
+  BUCKET_NAME = 'pp-consumer'
+  FILENAME = 'tmp/gef_db.mdb'
+
+  def self.mdb_from_s3
+    s3 = S3.new(BUCKET_NAME)
+    s3.download_from_bucket(filename: FILENAME)
+  end
+end
