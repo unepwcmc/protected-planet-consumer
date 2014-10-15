0f14999a835fab3773f8f95f818dbfecede2a6dc
Miguel Torres <migtorres@gmail.com>
2014-10-15 13:05:52 +0100

    adds seeds to gef_column_matches

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141015112753) do
+ActiveRecord::Schema.define(version: 20141015120330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "gef_column_matches", force: true do |t|
     t.string   "model_columns"
-    t.string   "type"
+    t.string   "column_type"
     t.text     "xls_columns"
     t.datetime "created_at"
     t.datetime "updated_at"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+source = File.join(Rails.root, 'lib', 'data', 'seeds','gef_columns_matching_table.csv')
+
+CSV.foreach(source, :headers => true) do |row|
+  GefColumnMatch.create!(row.to_hash)
+end
