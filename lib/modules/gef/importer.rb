9fd26286c52ad7f2f031c7d13c1a91810999f4da
Miguel Torres <migtorres@gmail.com>
2014-10-17 16:14:20 +0100

    adds find_fields method and test

--- lib/modules/gef/importer.rb
+++ lib/modules/gef/importer.rb
@@ -6,6 +6,17 @@ class Gef::Importer
     csv_table.from(1).map {|value| Hash[keys.zip(value)] }
   end
 
+  def self.find_fields protected_area
+    gef_protected_area = {}
+    protected_area.each do |k,v|
+       model_column = GefColumnMatch.select(:model_columns).where(xls_columns: k)
+       gef_protected_area.merge!(model_column.first['model_columns'].to_sym => v)
+    end
+    gef_protected_area
+  end
+
+  private
+
   def self.read_csv
     CSV.read(@filename)
   end
--- test/unit/gef/gef_importer_test.rb
+++ test/unit/gef/gef_importer_test.rb
@@ -1,19 +1,32 @@
 require 'test_helper'
+require 'csv'
 
 class TestGefImporter < ActiveSupport::TestCase
 
   # read csv
 
-  test '.convert_to_hash reads csv and converts to array of hashes' do 
+  test '.convert_to_hash reads csv and converts to array of hashes' do
     filename = 'long_tables.csv'
-    parsed_csv = [['name', 'age'], ['wolf', 10], ['dog', 20]]
+    parsed_csv = [['name in file', 'age'], ['wolf', 10], ['dog', 20]]
+
+    result = [{'name in file' => 'wolf', 'age' => 10}, {'name in file' => 'dog', 'age' => 20}]
     
     CSV.expects(:read).with(filename).returns(parsed_csv)
 
-    Gef::Importer.convert_to_hash(filename: filename)
+    assert_equal result, Gef::Importer.convert_to_hash(filename: filename)
   end
-    
-  # match
 
-  #populate model 
+  test '.find_fields creates a hash with columns of model and values' do
+
+    FactoryGirl.create(:gef_column_match, model_columns: 'pa_name_mett', xls_columns: 'name in file')
+    FactoryGirl.create(:gef_column_match, model_columns: 'research', xls_columns: 'Research')
+
+    protected_area = {'name in file' => 'wolf', 'Research' => 4}
+
+    result = {pa_name_mett:  'wolf', research: 4}
+
+    #GefProtectedArea.expects(:create).with(:pa_name_met).at_least(2)
+
+    assert_equal result, Gef::Importer.find_fields(protected_area)
+  end
 end
