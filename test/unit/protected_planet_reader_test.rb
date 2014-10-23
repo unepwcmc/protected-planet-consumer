c017460029153f923e1aca2302e321b1c967c56b
Miguel Torres <migtorres@gmail.com>
2014-10-23 18:55:17 +0100

    refactors pp_reader_test

--- test/unit/protected_planet_reader_test.rb
+++ test/unit/protected_planet_reader_test.rb
@@ -1,20 +1,19 @@
 class TestProtectedPlanetReader < ActiveSupport::TestCase
 
   def setup
-    Rails.application.secrets.protected_planet_api_url = 'http://my_website.com/api/'
+    Rails.application.secrets.protected_planet_api_url = 'http://mywebsite.com/api/'
   end
 
   test '.wdpa_id returns a hash with the given protected area from ProtectedPlanet API' do
 
-    json_file = File.read(File.join('test','fixtures','pp_area.json'))
+    json_file = File.read(File.join('test', 'fixtures', 'pp_area.json'))
 
-    stub_request(:get,'http://my_website.com/api/protected_areas?wdpa_id=1').
-      to_return(status: 200, body: json_file)
+    stub_request(:get,'http://mywebsite.com/api/protected_areas?wdpa_id=1').
+       to_return(status: 200, body: json_file, :headers => {})
 
     reader = ProtectedPlanetReader.new()
-    protected_area_hash = reader.wdpaid(id: 1)
-  
-    assert_equal 'Manbone', protected_area_hash[:name]
-  
+    protected_area_hash = reader.protected_area_from_wdpaid(id: 1)
+
+    assert_equal 'Killbear', protected_area_hash[:name]
   end
 end
