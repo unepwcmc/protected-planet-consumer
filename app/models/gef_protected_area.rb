054dc266a73f33ad8c22bfb339e7769a3b683da5
Miguel Torres <migtorres@gmail.com>
2014-10-27 17:14:46 +0000

    adds generate_data to GefProtectedArea model

--- app/models/gef_protected_area.rb
+++ app/models/gef_protected_area.rb
@@ -1,2 +1,9 @@
 class GefProtectedArea < ActiveRecord::Base
+  def generate_data
+    self.wdpa_id
+    consumer = Gef::Consumer.new
+    wdpa_api_data = consumer.api_data(wdpa_id: self.wdpa_id)
+    gef_data = self.attributes.symbolize_keys.except(:created_at, :updated_at)
+    gef_data.merge!(wdpa_api_data).compact
+  end
 end
--- test/models/gef_protected_area_test.rb
+++ test/models/gef_protected_area_test.rb
@@ -3,20 +3,27 @@ require 'test_helper'
 class GefProtectedAreaTest < ActiveSupport::TestCase
   test '.full_data returns a hash with all the data requested' do
 
-    FactoryGirl.create(:gef_protected_area, pa_name_mett: 'Mankind Boneless', gef_pmis_id: 8888)
+    current_time = Time.now
+    FactoryGirl.create(:gef_protected_area,
+      id: 1,
+      wdpa_id: 555999,
+      pa_name_mett: 'Mankind Boneless',
+      gef_pmis_id: 8888
+      )
 
     consumer_mock = mock
-    consumer_mock.expects(:api_data).returns(id: 1, wdpa_id: 555999,  pa_name_wdpa: 'Manbone')
+    consumer_mock.expects(:api_data).with(wdpa_id: 555999).returns(wdpa_id: 555999,  pa_name_wdpa: 'Manbone')
 
     Gef::Consumer.expects(:new).returns(consumer_mock)
 
     result = {
+      id: 1,
+      gef_pmis_id: '8888',
       wdpa_id: 555999,
-      pa_name_wdpa: 'Manbone',
       pa_name_mett: 'Mankind Boneless',
-      gef_pmis_id: 8888
+      pa_name_wdpa: 'Manbone'
     }
 
-    assert_equal result, GefProtectedArea.where(id: 1).generate_data
+    assert_equal result, GefProtectedArea.where(id: 1).first.generate_data
   end
 end
