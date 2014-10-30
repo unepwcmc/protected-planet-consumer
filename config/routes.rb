a27cec57aedc1b2dceb49847f58f587da277e6d2
Miguel Torres <migtorres@gmail.com>
2014-10-30 13:41:30 +0000

    starts to create a gef namespace

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
     resources :gef, only: :index
   end
 
+  namespace :gef do
+    resources :protected_areas
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
