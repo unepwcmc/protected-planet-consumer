0857eabae50a1b1af6e2365ba14a8de2283045c6
Miguel Torres <migtorres@gmail.com>
2014-10-31 16:37:21 +0000

    adds capybara to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,9 +5,14 @@ require 'rails/test_help'
 ActiveRecord::Migration.maintain_test_schema!
 require 'webmock/minitest'
 
-
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
+end
+
+class ActionDispatch::IntegrationTest
+  # Make the Capybara DSL available in all integration tests
+  include Capybara::DSL
+  Capybara.app = Rails.application
 end
 
 
