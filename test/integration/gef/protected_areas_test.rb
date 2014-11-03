96f21aced239afd57165054cbe9b65f4c7aef2fc
Miguel Torres <migtorres@gmail.com>
2014-11-03 16:28:22 +0000

    fixes protected areas view tests

--- test/integration/gef/protected_areas_test.rb
+++ test/integration/gef/protected_areas_test.rb
@@ -2,28 +2,41 @@ require 'test_helper'
 
 class Gef::ProtectedAreasTest < ActionDispatch::IntegrationTest
   test 'returns list of gef pas for given gef_pmis_id' do
+
     FactoryGirl.create(:gef_protected_area, gef_pmis_id: 1, pa_name_mett: 'Killbear', wdpa_id: 999888)
 
-    Gef::ProtectedArea.stubs(:generate_data).returns(wdpa_id: 1, gef_pmis_id: 1, name: 'Killbear', wdpa_id: 999888, wdpa_data: {name: 'Willbear'})
+    consumer_mock = mock
+    consumer_mock.expects(:api_data).with(wdpa_id: 999888).returns(wdpa_id: 1, gef_pmis_id: 1, name: 'Killbear', wdpa_id: 999888, wdpa_data: {name: 'Willbear'})
+
+    Gef::Consumer.expects(:new).returns(consumer_mock)
 
     get '/gef/1'
+
     assert_equal 200, response.status
   end
 
   test 'renders GEF Heading' do
+
     FactoryGirl.create(:gef_protected_area, gef_pmis_id: 1, pa_name_mett: 'Killbear', wdpa_id: 999888)
 
-    Gef::ProtectedArea.stubs(:generate_data).returns(wdpa_id: 1, gef_pmis_id: 1, name: 'Killbear', wdpa_id: 999888, wdpa_data: {name: 'Willbear'})
+    consumer_mock = mock
+    consumer_mock.expects(:api_data).with(wdpa_id: 999888).returns(wdpa_id: 999888, gef_pmis_id: 1, name: 'Killbear', wdpa_id: 999888, wdpa_data: {name: 'Willbear'})
 
-    get '/gef/1'
+    Gef::Consumer.expects(:new).returns(consumer_mock)
 
-    assert page.has_selector?('h1', text: 'GEF ID: #1'), 'h1 does not match'
+    visit '/gef/1'
+
+    assert page.has_selector?('h1', text: 'GEF ID #1'), 'h1 does not match'
   end
 
   test 'renders WDPA name' do
-FactoryGirl.create(:gef_protected_area, gef_pmis_id: 1, pa_name_mett: 'Killbear', wdpa_id: 999888)
 
-    Gef::ProtectedArea.stubs(:generate_data).returns(wdpa_id: 1, gef_pmis_id: 1, name: 'Killbear', wdpa_id: 999888, wdpa_data: {name: 'Willbear'})
+    FactoryGirl.create(:gef_protected_area, gef_pmis_id: 1, pa_name_mett: 'Killbear', wdpa_id: 999888)
+
+    consumer_mock = mock
+    consumer_mock.expects(:api_data).with(wdpa_id: 999888).returns(wdpa_id: 999888, gef_pmis_id: 1, name: 'Killbear', wdpa_id: 999888, wdpa_data: {name: 'Willbear'})
+
+    Gef::Consumer.expects(:new).returns(consumer_mock)
 
     get 'gef/1'
 
@@ -33,7 +46,10 @@ FactoryGirl.create(:gef_protected_area, gef_pmis_id: 1, pa_name_mett: 'Killbear'
   test 'renders WDPA ID' do
     FactoryGirl.create(:gef_protected_area, gef_pmis_id: 1, pa_name_mett: 'Killbear', wdpa_id: 999888)
 
-    Gef::ProtectedArea.stubs(:generate_data).returns(wdpa_id: 1, gef_pmis_id: 1, name: 'Killbear', wdpa_id: 999888, wdpa_data: {name: 'Willbear'})
+    consumer_mock = mock
+    consumer_mock.expects(:api_data).with(wdpa_id: 999888).returns(wdpa_id: 999888, gef_pmis_id: 1, name: 'Killbear', wdpa_id: 999888, wdpa_data: {name: 'Willbear'})
+
+    Gef::Consumer.expects(:new).returns(consumer_mock)
 
     get '/gef/1'
 
@@ -43,7 +59,10 @@ FactoryGirl.create(:gef_protected_area, gef_pmis_id: 1, pa_name_mett: 'Killbear'
   test 'renders Protected Planet Link' do
     FactoryGirl.create(:gef_protected_area, gef_pmis_id: 1, pa_name_mett: 'Killbear', wdpa_id: 999888)
 
-    Gef::ProtectedArea.stubs(:generate_data).returns(wdpa_id: 1, gef_pmis_id: 1, name: 'Killbear', wdpa_id: 999888, wdpa_data: {name: 'Willbear'})
+    consumer_mock = mock
+    consumer_mock.expects(:api_data).with(wdpa_id: 999888).returns(wdpa_id: 999888, gef_pmis_id: 1, name: 'Killbear', wdpa_id: 999888, wdpa_data: {name: 'Willbear'})
+
+    Gef::Consumer.expects(:new).returns(consumer_mock)
 
     visit '/gef/1'
 
