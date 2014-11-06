cf92c196e145c2f4aa41265f3ffb8e5eacc30e00
Miguel Torres <migtorres@gmail.com>
2014-11-06 15:19:23 +0000

    Adds new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
-
   namespace :gef do
     resources :protected_areas
   end
 
   get '/api/gef/:gef_pmis_id', to: 'api/gef#show', as: 'gef'
 
-  get '/gef/:gef_pmis_id', to: 'gef/protected_area#index', as: 'protected_areas'
+  get '/gef/area/:gef_pmis_id/wdpa_record', to: 'gef/wdpa_record#index', as: 'area_wdpa_records'
+
+  get '/gef/area/:gef_pmis_id/wdpa_record/:wdpa_id', to: 'gef/wdpa_record#show', as: 'area_wdpa_record'
 
 end
