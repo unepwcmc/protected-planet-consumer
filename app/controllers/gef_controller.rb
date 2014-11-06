299a6c40b07b50225092a6fbf3975f79fc5ecfc8
Miguel Torres <migtorres@gmail.com>
2014-11-06 18:54:23 +0000

    adds search page

--- app/controllers/gef_controller.rb
+++ app/controllers/gef_controller.rb
@@ -1,5 +1,8 @@
 class GefController < ApplicationController
   def index
-
+    if params[:description]
+      areas = Gef::Area.where(gef_pmis_id: params[:description]).first
+      redirect_to area_wdpa_records_path(gef_pmis_id: areas.gef_pmis_id)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
     resources :protected_areas
   end
 
-  get '/api/gef/:gef_pmis_id', to: 'api/gef#show', as: 'gef'
+  get '/gef/', to: 'gef#index', as: 'gef'
+
+  get '/gef/area/:gef_pmis_id',  to: 'gef/area#index', as: 'gef_area'
+
+  get '/api/gef/:gef_pmis_id', to: 'api/gef#index', as: 'gef_home'
 
   get '/gef/area/:gef_pmis_id/wdpa_record', to: 'gef/wdpa_record#index', as: 'area_wdpa_records'
 
