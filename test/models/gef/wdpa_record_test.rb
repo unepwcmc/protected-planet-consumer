23d27ae7b21bedc993547f7c475b8852d55b9a8f
Miguel Torres <migtorres@gmail.com>
2014-11-06 15:21:12 +0000

    adds record test

--- test/models/gef/wdpa_record_test.rb
+++ test/models/gef/wdpa_record_test.rb
@@ -1,14 +1,14 @@
 require 'test_helper'
 
 class Gef::WdpaRecordTest < ActiveSupport::TestCase
-  test '.full_data returns a hash with all the data requested' do
+  test '.generate_data returns a hash with all the data requested' do
 
-    current_time = Time.now
+    gef_area_1 = FactoryGirl.create(:gef_area, gef_pmis_id: 8888)
+
     FactoryGirl.create(:gef_wdpa_record,
       id: 1,
       wdpa_id: 555999,
-      pa_name_mett: 'Mankind Boneless',
-      gef_pmis_id: 8888
+      pa_name_mett: 'Mankind Boneless'
       )
 
     consumer_mock = mock
@@ -18,7 +18,6 @@ class Gef::WdpaRecordTest < ActiveSupport::TestCase
 
     result = {
       id: 1,
-      gef_pmis_id: '8888',
       wdpa_id: 555999,
       pa_name_mett: 'Mankind Boneless',
       pa_name_wdpa: 'Manbone'
