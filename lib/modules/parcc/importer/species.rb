5e047cc98bf3b3e4bbc33031b115b748ca881469
Miguel Torres <migtorres@gmail.com>
2014-11-14 18:04:29 +0000

    imports species

--- lib/modules/parcc/importer/species.rb
+++ lib/modules/parcc/importer/species.rb
@@ -2,6 +2,9 @@ require 'csv'
 
 class Parcc::Importer::Species
 
+  SPECIES_COLUMNS_MATCH = [:sensivity, :adaptability,
+                           :exposure_2025, :exposure_2055, :cc_vulnerable]
+
   def initialize filename: filename
     @filename = filename
     @csv_reader = CSV.read(@filename, headers: true, header_converters: :symbol)
@@ -14,6 +17,24 @@ class Parcc::Importer::Species
       Parcc::TaxonomicClass.create(name: taxo_class_name)
       taxo_class_id = Parcc::TaxonomicClass.where('name = ?', taxo_class_name).first.id
       Parcc::TaxonomicOrder.create(name: taxo_order_name, parcc_taxonomic_class_id: taxo_class_id)
+      species_input = species_data csv_record: row
+      Parcc::Species.create(species_input)
     end
+  end
+
+  def species_data csv_record: csv_record
+    species_hash = {}
+    csv_record.each do |k,v|
+      species_hash[:k] = v if SPECIES_COLUMNS_MATCH.include? k
+      species_hash[:iucn_cat] = v if k == :species_iucn_cat
+      species_hash[:name] = v if k == :species_binomial
+      if k == :cc_vulnerability
+        species_hash[:cc_vulnerable] = v == 'yes' ? true : false
+      elsif k == :species_order
+        order_id = Parcc::TaxonomicOrder.where('name = ?', v).first.id
+        species_hash[:parcc_taxonomic_order_id] = order_id
+      end
+    end
+    species_hash
   end
 end
--- test/unit/parcc/importer/species_test.rb
+++ test/unit/parcc/importer/species_test.rb
@@ -7,7 +7,7 @@ class TestParccImporterSpecies < ActiveSupport::TestCase
 
     filename = 'huge_csv'
 
-    response = {species_taxon: 'Bird', species_order: 'Nematelmintes'}
+    response = {species_taxon: 'Bird', species_order: 'Nematelmintes', species_binomial: 'Unepus Wicimesensis', cc_vulnerability: 'no', species_iucn_cat: 'VIII'}
 
     csv_mock = mock
 
@@ -18,15 +18,21 @@ class TestParccImporterSpecies < ActiveSupport::TestCase
     Parcc::TaxonomicClass.expects(:create).with(name: 'Bird')
 
     bird_id_mock = mock
-
     bird_id_mock.expects(:id).returns(1)
-
     bird_mock = mock
     bird_mock.expects(:first).returns(bird_id_mock)
 
-    Parcc::TaxonomicClass.expects(:where).with('name = ?', 'Bird').returns(bird_mock)
+    nema_id_mock = mock
+    nema_id_mock.expects(:id).returns(2)
+    nema_mock = mock
+    nema_mock.expects(:first).returns(nema_id_mock)
 
-    Parcc::TaxonomicOrder.expects(:create).with(name: 'Nematelmintes', parcc_taxonomic_class_id: 1).once
+    Parcc::TaxonomicClass.expects(:where).with('name = ?', 'Bird').returns(bird_mock)
+    Parcc::TaxonomicOrder.expects(:where).with('name = ?', 'Nematelmintes').returns(nema_mock)
+
+    Parcc::TaxonomicOrder.expects(:create).with(name: 'Nematelmintes', parcc_taxonomic_class_id: 1)
+
+    Parcc::Species.expects(:create).with(name: 'Unepus Wicimesensis', parcc_taxonomic_order_id: 2, cc_vulnerable: false, iucn_cat: 'VIII')
 
     importer = Parcc::Importer::Species.new filename: filename
     importer.import_taxo
