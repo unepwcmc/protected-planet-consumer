86f7bfded89e343df525d174e982233e315b3743
Miguel Torres <migtorres@gmail.com>
2014-11-14 10:10:30 +0000

    iadds contraints to parcc models

--- app/models/parcc/species.rb
+++ app/models/parcc/species.rb
@@ -1,2 +1,5 @@
 class Parcc::Species < ActiveRecord::Base
+  validates_uniqueness_of :name
+
+  belongs_to :parcc_taxonomic_order, class_name: 'Parcc::TaxonomicOrder', foreign_key: :parcc_taxonomic_order_id
 end
--- app/models/parcc/taxonomic_class.rb
+++ app/models/parcc/taxonomic_class.rb
@@ -1,2 +1,5 @@
 class Parcc::TaxonomicClass < ActiveRecord::Base
+  validates_uniqueness_of :name
+
+  has_many :taxonomic_orders, class_name: 'Parcc::TaxonomicOrder', foreign_key: :parcc_taxonomic_class_id
 end
