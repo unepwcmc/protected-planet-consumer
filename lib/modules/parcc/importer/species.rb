c8f221daccaa8d424ce981e46bdf0d7286806a6a
Miguel Torres <migtorres@gmail.com>
2014-11-14 12:12:59 +0000

    adds class and order importer

--- lib/modules/parcc/importer/species.rb
+++ lib/modules/parcc/importer/species.rb
@@ -1,9 +1,19 @@
 require 'csv'
 
 class Parcc::Importer::Species
+
   def initialize filename: filename
     @filename = filename
-    @csv_reader = CSV.read(@filename, headers: true)
+    @csv_reader = CSV.read(@filename, headers: true, header_converters: :symbol)
   end
 
+  def import_taxo
+    @csv_reader.each do |row|
+      taxo_class_name = row[:species_taxon]
+      taxo_order_name = row[:species_order]
+      Parcc::TaxonomicClass.create(name: taxo_class_name)
+      taxo_class_id = Parcc::TaxonomicClass.where('name = ?', taxo_class_name).first.id
+      Parcc::TaxonomicOrder.create(name: taxo_order_name, parcc_taxonomic_class_id: taxo_class_id)
+    end
+  end
 end
--- test/unit/parcc/importer/species_test.rb
+++ test/unit/parcc/importer/species_test.rb
@@ -2,33 +2,47 @@ require 'test_helper'
 require 'csv'
 
 class TestParccImporterSpecies < ActiveSupport::TestCase
-  test '.import_taxo_class imports classes' do
-
-    filename = 'huge_csv'
-
-    response = {'SPECIES_TAXON' => ['Bird', 'Fish', 'Bird']}
-
-    CSV.expects(:read).with(filename, headers: true).returns(response)
-    
-    Parcc::TaxonomicClass.expects(:create).with(name: 'Bird').once
-    Parcc::TaxonomicClass.expects(:create).with(name: 'Fish')
-
-    importer = Parcc::Importer::Species.new filename
-    importer.import_taxo_class
-  end
 
   test '.import_taxo_order imports classes' do
 
     filename = 'huge_csv'
 
-    response = {'SPECIES_ORDER' => ['Nematelmintes', 'Platielmintes', 'Platielmintes']}
+    response = [
+                {species_taxon: 'Bird', species_order: 'Nematelmintes'},
+                {species_taxon: 'Bird', species_order: 'Nematelmintes'},
+                {species_taxon: 'Bird', species_order: 'Platielmintes'},
+                {species_taxon: 'Fish', species_order: 'Codielmintes'}
+               ]
+   
+    csv_mock = mock
 
-    CSV.expects(:read).with(filename, headers: true).returns(response)
+    csv_mock.expects(:each).multiple_yields(response)
+
+    CSV.expects(:read).with(filename, headers: true, header_converters: :symbol).returns(csv_mock)
     
-    Parcc::TaxonomicClass.expects(:create).with(name: 'Nematelmintes')
-    Parcc::TaxonomicClass.expects(:create).with(name: 'Platielmintes').once
+    Parcc::TaxonomicClass.expects(:create).with(name: 'Bird').once
+    Parcc::TaxonomicClass.expects(:create).with(name: 'Fish')
 
-    importer = Parcc::Importer::Species.new filename
-    importer.import_taxo_order
+    fish_id_mock = mock
+    bird_id_mock = mock
+
+    bird_id_mock.expects(:id).returns(1)
+    fish_id_mock.expects(:id).returns(2)
+
+    fish_mock = mock
+    fish_mock.expects(:first).returns(fish_id_mock)
+
+    bird_mock = mock
+    bird_mock.expects(:first).returns(bird_id_mock)
+
+    Parcc::TaxonomicClass.expects(:where).with('name = ?', 'Bird').returns(bird_mock)
+    Parcc::TaxonomicClass.expects(:where).with('name = ?', 'Fish').returns(fish_mock)
+
+    Parcc::TaxonomicOrder.expects(:create).with(name: 'Nematelmintes', parcc_taxonomic_class_id: 1).once
+    Parcc::TaxonomicOrder.expects(:create).with(name: 'Platielmintes', parcc_taxonomic_class_id: 1)
+    Parcc::TaxonomicOrder.expects(:create).with(name: 'Codielmintes', parcc_taxonomic_class_id: 2)
+
+    importer = Parcc::Importer::Species.new filename: filename
+    importer.import_taxo
   end
 end
