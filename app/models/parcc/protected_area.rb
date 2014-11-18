dc42d12157f722f7ad5f22c9d5f44907f11c0a60
Miguel Torres <migtorres@gmail.com>
2014-11-18 11:44:55 +0000

    adds class name to parcc species relationship

--- app/models/parcc/protected_area.rb
+++ app/models/parcc/protected_area.rb
@@ -1,5 +1,5 @@
 class Parcc::ProtectedArea < ActiveRecord::Base
   validates_uniqueness_of :parcc_id
   has_many :parcc_species_turnovers, class_name: 'Parcc::SpeciesTurnover', foreign_key: :parcc_protected_area_id
-  has_many :parcc_species, through: :parcc_species_protected_areas
+  has_many :parcc_species, class_name: 'Parcc::Species', through: :parcc_species_protected_areas
 end
