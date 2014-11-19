5a66e8b59a559d6419b88d38b00b3d0c9e48687e
Miguel Torres <migtorres@gmail.com>
2014-11-19 16:24:27 +0000

    adds new routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get '/gef/', to: 'gef#index', as: 'gef'
 
-  get '/gef/area/:gef_pmis_id',  to: 'gef/area#index', as: 'gef_area'
+  get '/gef/area/:gef_pmis_id',  to: 'gef/area#show', as: 'gef_area'
 
   get '/gef/area/:gef_pmis_id/pame-record', to: 'gef/pame_record#index', as: 'pame_records'
 
