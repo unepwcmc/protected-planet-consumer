fcd86807a2ccadccd13c06f47d6e72405ee0d920
Miguel Torres <migtorres@gmail.com>
2014-11-21 09:45:38 +0000

    adds links to pame index page

--- app/controllers/gef/pame_record_controller.rb
+++ app/controllers/gef/pame_record_controller.rb
@@ -1,7 +1,10 @@
 class Gef::PameRecordController < ApplicationController
 
   def index
-    @pame_assessments =  Gef::WdpaRecord.includes(:gef_pame_records).where('wdpa_id = ?', params[:wdpa_id])
-    debugger
+    @pame_assessments =  Gef::WdpaRecord.includes(:gef_pame_records, :gef_area).where('wdpa_id = ?', params[:wdpa_id])
+  end
+
+  def show
+
   end
 end
--- test/integration/gef/pame_records_index_test.rb
+++ test/integration/gef/pame_records_index_test.rb
@@ -31,4 +31,17 @@ class Gef::PameRecordsIndexTest < ActionDispatch::IntegrationTest
     assert_match /888999/, @response.body
 
   end
+
+  test 'has links to pame show page' do
+    gef_area = FactoryGirl.create(:gef_area, gef_pmis_id: 666777)
+
+    wdpa_area = FactoryGirl.create(:gef_wdpa_record, gef_area: gef_area, wdpa_id: 333444)
+
+    FactoryGirl.create(:gef_pame_record, gef_wdpa_record: wdpa_area, mett_original_uid: 999888)
+
+    visit '/gef/area/666777/wdpa-record/333444/pame-record'
+
+    assert page.has_link?('999888', /999888/)
+
+  end
 end
