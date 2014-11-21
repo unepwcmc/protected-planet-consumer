5df71bcb93988224bada962cfa7d330dfee75b60
Miguel Torres <migtorres@gmail.com>
2014-11-21 09:01:04 +0000

    adds new routes with wdpa_id

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   get '/gef/area/:gef_pmis_id',  to: 'gef/area#show', as: 'gef_area'
 
-  get '/gef/area/:gef_pmis_id/pame-record', to: 'gef/pame_record#index', as: 'pame_records'
+  get '/gef/area/:gef_pmis_id/wdpa-record/:wdpa_id/pame-record', to: 'gef/pame_record#index', as: 'pame_records'
 
-  get '/gef/area/:gef_pmis_id/pame-record/:mett_original_uid', to: 'gef/pame_record#show', as: 'pame_record'
+  get '/gef/area/:gef_pmis_id/wdpa-record/:wdpa_id/pame-record/:mett_original_uid', to: 'gef/pame_record#show', as: 'pame_record'
 end
