32d0d27f8988b769a88d75e1fb53fec80aa1fe3f
Miguel Torres <migtorres@gmail.com>
2014-11-21 09:28:25 +0000

    integration test for show now passes

--- test/integration/gef/pame_records_index_test.rb
+++ test/integration/gef/pame_records_index_test.rb
@@ -3,7 +3,11 @@ require 'test_helper'
 class Gef::PameRecordsIndexTest < ActionDispatch::IntegrationTest
   test 'returns a page for given gef_id' do
 
-    gef_area_1 = FactoryGirl.create(:gef_area, gef_pmis_id: 666777)
+    gef_area = FactoryGirl.create(:gef_area, gef_pmis_id: 666777)
+
+    wdpa_area = FactoryGirl.create(:gef_wdpa_record, gef_area: gef_area, wdpa_id: 333444)
+
+    FactoryGirl.create(:gef_pame_record, gef_wdpa_record: wdpa_area, mett_original_uid: 999888)
 
     get '/gef/area/666777/wdpa-record/333444/pame-record'
 
@@ -24,7 +28,6 @@ class Gef::PameRecordsIndexTest < ActionDispatch::IntegrationTest
     get '/gef/area/666777/wdpa-record/333444/pame-record'
 
     assert_match /999888/, @response.body
-
     assert_match /888999/, @response.body
 
   end
