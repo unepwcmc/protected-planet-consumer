20c14dedd8c417e40b1378161f94d4f043075fb6
Miguel Torres <migtorres@gmail.com>
2014-12-09 19:29:15 +0000

    validates name

--- app/models/gef/pame_name.rb
+++ app/models/gef/pame_name.rb
@@ -1,4 +1,5 @@
 class Gef::PameName < ActiveRecord::Base
   has_many :gef_wdpa_records, class_name: 'Gef::WdpaRecord', foreign_key: :gef_pame_name_id
   has_many :gef_pame_records, class_name: 'Gef::PameRecord', foreign_key: :gef_pame_name_id
+  validates_uniqueness_of :name
 end
