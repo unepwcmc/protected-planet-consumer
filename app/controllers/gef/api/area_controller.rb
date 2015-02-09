c606126b378d68d8e205642be14eb6698772c9b9
Miguel Torres <migtorres@gmail.com>
2015-02-09 18:02:39 +0000

    adds wdpa_record to api

--- app/controllers/gef/api/area_controller.rb
+++ app/controllers/gef/api/area_controller.rb
@@ -2,7 +2,7 @@ class Gef::Api::AreaController < ApplicationController
   def index
     areas = Gef::Area.all
     if gef_pmis_id = params[:id]
-      areas = areas.where('gef_pmis_id = ?', gef_pmis_id)
+      areas = areas.where('gef_pmis_id = ?', gef_pmis_id).first.generate_data
     end
     render json: areas, status: 200
   end
--- test/integration/gef/api_areas_test.rb
+++ test/integration/gef/api_areas_test.rb
@@ -3,11 +3,20 @@ require 'test_helper'
 class Gef::ApiAreasTest < ActionDispatch::IntegrationTest
   test 'returns protected areas for a given gef_id' do
 
-    gef_area = FactoryGirl.create(:gef_area, gef_pmis_id: 666777)
+    gef_area_1 = FactoryGirl.create(:gef_area, gef_pmis_id: 666777)
+    wdpa_area_1 = FactoryGirl.create(:gef_wdpa_record, gef_area: gef_area_1, wdpa_id: 333444)
+    FactoryGirl.create(:gef_pame_record, gef_area: gef_area_1, gef_wdpa_record: wdpa_area_1, mett_original_uid: 999888)
 
-    wdpa_area = FactoryGirl.create(:gef_wdpa_record, gef_area: gef_area, wdpa_id: 333444)
+    gef_area_2 = FactoryGirl.create(:gef_area, gef_pmis_id: 888999)
+    wdpa_area_2 = FactoryGirl.create(:gef_wdpa_record, gef_area: gef_area_2, wdpa_id: 111222)
+    FactoryGirl.create(:gef_pame_record, gef_area: gef_area_2, gef_wdpa_record: wdpa_area_2, mett_original_uid: 222333)
 
-    FactoryGirl.create(:gef_pame_record, gef_area: gef_area, gef_wdpa_record: wdpa_area, mett_original_uid: 999888)
+
+
+    Gef::WdpaRecord.expects(:wdpa_name)
+                   .with(gef_pmis_id: 666777)
+                   .returns([{ wdpa_name: 'Willbear', wdpa_id: 333444,
+                                protected_planet_url: "http://alpha.protectedplanet.net/333444" }])
 
     get '/gef/api/area?id=666777'
 
@@ -15,7 +24,12 @@ class Gef::ApiAreasTest < ActionDispatch::IntegrationTest
     
     areas = JSON.parse(response.body, symbolize_names: true)
     gef_pmis_id = areas.collect{ |z| z[:gef_pmis_id]}
+    wdpa_id = areas.collect{ |z| z[:wdpa_id]}
+
     assert_includes gef_pmis_id, 666777
+    assert_includes wdpa_id, 333444
+
+    refute_includes gef_pmis_id, 888999
 
   end
 end
