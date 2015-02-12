789706b1622821b044046ae6f16ad3d4f6d415a4
Miguel Torres <migtorres@gmail.com>
2015-02-12 17:27:01 +0000

    adds to_csv method

--- app/models/gef/area.rb
+++ app/models/gef/area.rb
@@ -18,6 +18,23 @@ class Gef::Area < ActiveRecord::Base
     api_data
   end
 
+  def to_csv
+    csv = []
+    csv_headers = []
+    wdpa_data = query_wdpa gef_pmis_id: self.gef_pmis_id
+    wdpa_data.each do |protected_area|
+      pame_records = pame_records(wdpa_id: protected_area[:wdpa_id], gef_pmis_id: gef_pmis_id)
+      pame_records.each do |assessment|
+        csv_hash = assessment.attributes.merge! protected_area
+        csv_hash.except!('id', 'created_at', 'updated_at', 'gef_wdpa_record_id', 'gef_area_id')
+        csv << csv_hash.values
+        csv_headers = csv_hash.keys
+      end
+    end
+
+    csv.insert(0,csv_headers)
+  end
+
   private
 
   def query_wdpa gef_pmis_id: gef_pmis_id
@@ -26,10 +43,7 @@ class Gef::Area < ActiveRecord::Base
   end
 
   def assessments wdpa_id: wdpa_id, gef_pmis_id: gef_pmis_id
-    pame_records = Gef::PameRecord.joins(:gef_wdpa_record, :gef_area)
-                    .where('gef_wdpa_records.wdpa_id = ? AND gef_areas.gef_pmis_id = ?',
-                            wdpa_id, gef_pmis_id)
-                    .order('mett_original_uid ASC')
+    pame_records = pame_records (wdpa_id: wdpa_id, gef_pmis_id: gef_pmis_id)
 
     pame_hash = pame_records.map{ |record| record.attributes }
     pame_hash.each do |record|
@@ -38,5 +52,12 @@ class Gef::Area < ActiveRecord::Base
       record.except!(:id, :created_at, :updated_at, :gef_wdpa_record_id, :gef_area_id)
     end
     pame_hash
+  end
+
+  def pame_records wdpa_id: wdpa_id, gef_pmis_id: gef_pmis_id
+    pame_records = Gef::PameRecord.joins(:gef_wdpa_record, :gef_area)
+                .where('gef_wdpa_records.wdpa_id = ? AND gef_areas.gef_pmis_id = ?',
+                        wdpa_id, gef_pmis_id)
+                .order('mett_original_uid ASC')
   end
 end
--- test/models/gef/area_test.rb
+++ test/models/gef/area_test.rb
@@ -48,6 +48,25 @@ class Gef::AreaTest < ActiveSupport::TestCase
       }]
 
     assert_equal result, Gef::Area.where(gef_pmis_id: 8888).first.generate_api_data
+  end
 
+  test '.to_csv returns all csv data' do
+
+    gef_area_1 = FactoryGirl.create(:gef_area, gef_pmis_id: 8888)
+    wdpa_area_1 = FactoryGirl.create(:gef_wdpa_record, gef_area: gef_area_1, wdpa_id: 333444)
+    FactoryGirl.create(:gef_pame_record, gef_area: gef_area_1, gef_wdpa_record: wdpa_area_1,
+                        mett_original_uid: 888999, assessment_year: 2003)
+
+
+    Gef::WdpaRecord.expects(:wdpa_name)
+                   .with(gef_pmis_id: 8888)
+                   .returns([wdpa_id: 333444, wdpa_name: 'Manbone',
+                             protected_planet_url: 'http://alpha.protectedplanet.net/555999'])
+
+    result = [["mett_original_uid", "gef_pmis_id", "wdpa_name" ],[888999, 8888, 'Manbone']]
+
+    request =  Gef::Area.where(gef_pmis_id: 8888).first.to_csv
+    assert_equal result[0][0], request[0][0]
+    assert_equal result[1][0], request[1][0]
   end
 end
