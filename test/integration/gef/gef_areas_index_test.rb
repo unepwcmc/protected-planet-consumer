57356ce6eebcd7cedc6fc8f29d0a0aa2b3026695
Miguel Torres <migtorres@gmail.com>
2015-02-19 16:02:27 +0000

    fixes csv link

--- test/integration/gef/gef_areas_index_test.rb
+++ test/integration/gef/gef_areas_index_test.rb
@@ -6,7 +6,7 @@ class Gef::AreaIndexTest < ActionDispatch::IntegrationTest
 
     visit '/gef'
 
-    assert page.has_link?('Download All Areas in CSV format', :href => '/public/gef/gef_full_database.csv'),
+    assert page.has_link?('Download All Areas in CSV format', :href => '/gef/gef_full_database.csv'),
       'Has no csv downlad link'
 
   end
