020edb47a6ecf8703a3e82d502ac864c6c7c0bf8
Miguel Torres <migtorres@gmail.com>
2015-03-03 15:17:37 +0000

    adds relationships between countries regions and wdpa_records

--- app/models/gef/country.rb
+++ app/models/gef/country.rb
@@ -1,2 +1,5 @@
 class Gef::Country < ActiveRecord::Base
+  belongs_to :gef_region, class_name: 'Gef::Region', foreign_key: :gef_region_id
+  has_many :gef_wdpa_records, class_name: 'Gef::WdpaRecord', through: :gef_country_wdpa_records
+  has_many :gef_country_wdpa_records, class_name: 'Gef::CountryWdpaRecord', foreign_key: :gef_country_id
 end
--- app/models/gef/country_wdpa_record.rb
+++ app/models/gef/country_wdpa_record.rb
@@ -1,2 +1,4 @@
 class Gef::CountryWdpaRecord < ActiveRecord::Base
+  belongs_to :gef_country, class_name: 'Gef::Country', foreign_key: :gef_country_id
+  belongs_to :gef_wdpa_record, class_name: 'Gef::WdpaRecord', foreign_key: :gef_wdpa_record_id
 end
