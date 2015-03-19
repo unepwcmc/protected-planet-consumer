aa5d69a5d30c99fa8a8a15056f7653b405cb27d9
Miguel Torres <migtorres@gmail.com>
2015-03-19 12:26:46 +0000

    fixxes missing primary biome

--- app/models/gef/search.rb
+++ app/models/gef/search.rb
@@ -9,12 +9,11 @@ class Gef::Search < ActiveRecord::Base
 private
 
   def find_areas
-    areas =  Gef::WdpaRecord.select('*').joins(:gef_area, gef_countries: :gef_region)
-    areas = areas.where("gef_areas.gef_pmis_id = ?", gef_pmis_id) if gef_pmis_id.present?
+    areas =  Gef::WdpaRecord.select('*').joins(gef_area: :gef_pame_records, gef_countries: :gef_region)
+    areas = areas.where('gef_areas.gef_pmis_id = ?', gef_pmis_id) if gef_pmis_id.present?
     areas = areas.where('gef_country_id = ?', gef_country_id) if gef_country_id.present?
     areas = areas.where('gef_countries.gef_region_id = ?', gef_region_id) if gef_region_id.present?
-    areas = areas.where(primary_biome: primary_biome) if primary_biome.present?     
-    areas = areas.where(wdpa_id: wdpa_id) if wdpa_id.present?
+    areas = areas.where('gef_pame_records.primary_biome = ?', primary_biome) if primary_biome.present?
     areas = areas.where(wdpa_name: wdpa_name) if wdpa_name.present?
 
     all_data = []
