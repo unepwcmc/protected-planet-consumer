59c82219ff3e5ea2ab38caa8d148338b024efe28
Miguel Torres <migtorres@gmail.com>
2015-03-23 10:48:21 +0000

    writes search queries in a more rails way

--- app/models/gef/search.rb
+++ app/models/gef/search.rb
@@ -9,20 +9,22 @@ class Gef::Search < ActiveRecord::Base
 private
 
   def find_areas
-    areas =  Gef::WdpaRecord.select('*').joins(gef_area: :gef_pame_records, gef_countries: :gef_region)
-    areas = areas.where('gef_areas.gef_pmis_id = ?', gef_pmis_id) if gef_pmis_id.present?
-    areas = areas.where('gef_country_id = ?', gef_country_id) if gef_country_id.present?
-    areas = areas.where('gef_countries.gef_region_id = ?', gef_region_id) if gef_region_id.present?
-    areas = areas.where('gef_pame_records.primary_biome = ?', primary_biome) if primary_biome.present?
+    areas =  Gef::WdpaRecord.joins(gef_area: :gef_pame_records, gef_countries: :gef_region)
+    areas = areas.where(gef_areas: {gef_pmis_id: gef_pmis_id}) if gef_pmis_id.present?
+    areas = areas.where(gef_countries: {id: gef_country_id}) if gef_country_id.present?
+    areas = areas.where(gef_countries: {gef_region_id: gef_region_id}) if gef_region_id.present?
+    areas = areas.where(gef_pame_records: {primary_biome: primary_biome}) if primary_biome.present?
     areas = areas.where(wdpa_name: wdpa_name) if wdpa_name.present?
 
     all_data = []
     areas.each do |pa|
       protected_area = pa.attributes.symbolize_keys!
+      protected_area.merge!( pa.gef_area.attributes.symbolize_keys! )
       protected_area[:protected_planet_url] = protected_planet_url wdpa_id: protected_area[:wdpa_id]
       protected_area[:wdpa_name] = protected_area[:wdpa_exists] ? protected_area[:wdpa_name] : pa.name
       all_data << protected_area
     end
+    all_data.uniq!
     all_data
   end
 
