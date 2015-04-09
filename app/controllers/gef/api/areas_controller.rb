430636965a9312ae43cea6279840eff16ca6933f
Miguel Torres <migtorres@gmail.com>
2015-04-09 14:56:17 +0100

    adds navbar and footer

--- app/controllers/gef/api/areas_controller.rb
+++ app/controllers/gef/api/areas_controller.rb
@@ -1,7 +1,11 @@
 class Gef::Api::AreasController < ApplicationController
   def index
-    if gef_pmis_id = params[:id]
-      areas = Gef::Area.where('gef_pmis_id = ?', gef_pmis_id).first.generate_api_data
+    check_params = params.values.map{ |param| true if not param.blank? }
+
+    if check_params.include? true
+      debugger
+
+      # areas = Gef::Area.where('gef_pmis_id = ?', gef_pmis_id).first.generate_api_data
     end
     render json: areas, status: 200
   end
--- test/integration/gef/api_areas_test.rb
+++ test/integration/gef/api_areas_test.rb
@@ -3,37 +3,30 @@ require 'test_helper'
 class Gef::ApiAreasTest < ActionDispatch::IntegrationTest
   test 'returns protected areas for a given gef_id' do
 
-    gef_area_1 = FactoryGirl.create(:gef_area, gef_pmis_id: 666777)
-    wdpa_area_1 = FactoryGirl.create(:gef_wdpa_record, gef_area: gef_area_1, wdpa_id: 333444)
-    FactoryGirl.create(:gef_pame_record, gef_area: gef_area_1, gef_wdpa_record: wdpa_area_1, mett_original_uid: 999888)
+    gef_area_1 = FactoryGirl.create(:gef_area, gef_pmis_id: 888999)
 
-    Gef::WdpaRecord.expects(:wdpa_name)
-                   .with(gef_pmis_id: 666777)
-                   .returns([{ wdpa_name: 'Willbear', wdpa_id: 333444,
-                                protected_planet_url: "http://alpha.protectedplanet.net/333444" }])
+    gef_region_1 = FactoryGirl.create(:gef_region, name: 'Manarctica')
 
-    Gef::PameRecord.expects(:data_list)
-                   .with(mett_original_uid: 999888,
-                         wdpa_id: 333444)
-                   .returns(gef_pmis_id: 666777, wdpa_id: 333444, assessment_year: 2003,
-                            budget_project_type: 'Given', budget_recurrent_type: 'Given',
-                            mett_original_uid: 999888)
+    gef_country_1 = FactoryGirl.create(:gef_country, name: 'Manboneland', gef_region: gef_region_1)
 
-    get '/gef/api/areas?id=666777'
+    gef_name_1 = FactoryGirl.create(:gef_pame_name, name: 'Manbone')
+
+    wdpa_record_1 = FactoryGirl.create(:gef_wdpa_record, gef_area: gef_area_1, 
+                                      gef_pame_name: gef_name_1, wdpa_id: 999888)
+
+    FactoryGirl.create(:gef_pame_record, gef_wdpa_record: wdpa_record_1, 
+                        gef_area: gef_area_1, gef_pame_name: gef_name_1, 
+                        primary_biome: 'Biome', legal_status: 'legal')
+
+    get '/gef/api/areas?gef_pmis_id=888999&primary_biome=Biome&country=Manboneland&region=Manarctica&wdpa_id=999888'
 
     assert response.success?
     
     areas = JSON.parse(response.body, symbolize_names: true)
 
-    gef_pmis_id = areas[0][:gef_pmis_id]
-    wdpa_id = areas[0][:wdpa_id]
-    mett_original_uid = areas[0][:assessments][0][:mett_original_uid]
-    budget_project_type = areas[0][:assessments][0][:budget_project_type]
+    legal_status = areas[0][:assessments][0][:budget_project_type]
 
-    assert_equal gef_pmis_id, 666777
-    assert_equal wdpa_id, 333444
-    assert_equal mett_original_uid, 999888
-    assert_equal budget_project_type, 'Given'
+    assert_equal legal_status, 'legal'
 
   end
 end
