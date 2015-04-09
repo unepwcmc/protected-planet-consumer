901cc875064e04dbfd456cae03500d79e041d166
Miguel Torres <migtorres@gmail.com>
2015-04-09 17:23:04 +0100

    changes staging environment to png

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
+  config.assets.precompile =  ['*.png', '*.js', '*.css', '*.css.erb']
 
   # Generate digests for assets URLs.
   config.assets.digest = true
