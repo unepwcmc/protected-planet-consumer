c7097e08410cafca5b666e99a13b09c140b520d4
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-04-28 12:29:15 +0100

    refactoring in turnover

--- lib/modules/parcc/importer/turnover.rb
+++ lib/modules/parcc/importer/turnover.rb
@@ -1,62 +1,66 @@
 require 'csv'
 
 class Parcc::Importer::Turnover
-  
-  MATCH_COLUMNS = {parcc_id: '', name: 'name', iso_3: 'country',
-                   poly_id: 'polyID', designation: 'designation',
-                   geom_type: 'point', iucn_cat: 'iucn_cat',
-                   wdpa_id: 'WDPAID'}
-
   STATS = ['median', 'upper', 'lower']
+  MATCH_COLUMNS = {
+    parcc_id: '',
+    name: 'name',
+    iso_3: 'country',
+    poly_id: 'polyID',
+    designation: 'designation',
+    geom_type: 'point',
+    iucn_cat: 'iucn_cat',
+    wdpa_id: 'WDPAID'
+  }
 
   def import
-    files = list_files
-    create_pas file_path: files[0]
-    files.each do |file|
-      populate_values file_path: file
-    end
+    create_pas files.first
+    files.each { |file| populate_values file }
   end
 
-  def create_pas file_path: file_path
-    protected_areas = read_csv file_path: file_path
+  def create_pas file_path
+    protected_areas = read_csv(file_path)
+
     protected_areas.each do |pa|
-      pa_to_create = {}
-      MATCH_COLUMNS.each do |final, original|
-        pa_to_create[final] = pa[original] if pa[original]
+      pa_props = MATCH_COLUMNS.each_with_object({}) do |(final, original), props|
+        props[final] = pa[original] if pa[original]
       end
-      Parcc::ProtectedArea.create pa_to_create
+
+      Parcc::ProtectedArea.create(pa_props)
     end
   end
 
-  def populate_values file_path: file_path
-    filename_splitted = File.basename(file_path).split
-    csv_values = read_csv file_path: file_path  
-    csv_values.each do |pa|
-      values_to_populate = {}
-      values_to_populate[:taxonomic_class] = filename_splitted[0]
-      values_to_populate[:year] = filename_splitted[3]
+  def populate_values file_path
+    split_filename = File.basename(file_path).split
+
+    read_csv(file_path).each do |pa|
+      parcc_props = {
+        taxonomic_class: split_filename.first,
+        year: split_filename[3]
+      }
+
       pa.each do |k,v|
         if STATS.include? k
-          values_to_populate[:stat] = k
-          values_to_populate[:value] = v
-          create_turnover parcc_values:values_to_populate,  parcc_id: pa['']
+          parcc_props[:stat] = k
+          parcc_props[:value] = v
+          create_turnover parcc_props, pa['']
         end
       end
     end
   end
 
-  def create_turnover parcc_values: parcc_values, parcc_id: parcc_id
-    pa = Parcc::ProtectedArea.where(parcc_id: parcc_id).first
+  def create_turnover parcc_values, parcc_id
+    pa = Parcc::ProtectedArea.where(parcc_id: parcc_id).select(:id).first
+
     parcc_values.merge!(parcc_protected_area_id: pa.id)
     Parcc::SpeciesTurnover.create parcc_values
   end
 
-  def list_files
-    Dir['lib/data/parcc/*']
+  def files
+    @files ||= Dir['lib/data/parcc/*']
   end
 
-
-  def read_csv file_path: file_path
-    CSV.read(file_path, headers: true)
+  def read_csv file_path
+    CSV.foreach(file_path, headers: true)
   end
 end
