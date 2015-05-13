ce26379afe4f84e4121680933a219c2ef3a3d946
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-05-13 17:08:23 +0100

    change case of csv keys in counts importer

--- lib/modules/parcc/importers/species/counts.rb
+++ lib/modules/parcc/importers/species/counts.rb
@@ -1,12 +1,13 @@
 class Parcc::Importers::Species::Counts < Parcc::Importers::Base
   def import
     csv_reader.each do |record|
-      protected_area = fetch_protected_area(record[:WDPA_ID])
+      protected_area = fetch_protected_area(record[:wdpa_id])
+      next unless protected_area
 
       add_counts_to_pa(protected_area, {
-        count_total_species: record[:COUNT_TOTAL_SPECIES],
-        percentage_vulnerable_species: record[:PERCENT_CC_VULNERABLE_SPECIES],
-        count_vulnerable_species: record[:COUNT_CC_VULNERABLE_SPECIES]
+        count_total_species: record[:count_total_species],
+        percentage_vulnerable_species: record[:percent_cc_vulnerable_species],
+        count_vulnerable_species: record[:count_cc_vulnerable_species]
       })
     end
   end
