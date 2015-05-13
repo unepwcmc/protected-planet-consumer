0948ba2a4035123b0044afa4ad4cc846dac746c1
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-05-13 17:02:54 +0100

    fix parcc_species_protected_areas relation

--- db/migrate/20141117131304_create_parcc_species_protected_areas.rb
+++ db/migrate/20141117131304_create_parcc_species_protected_areas.rb
@@ -2,7 +2,7 @@ class CreateParccSpeciesProtectedAreas < ActiveRecord::Migration
   def change
     create_table :parcc_species_protected_areas do |t|
       t.integer :parcc_species_id
-      t.integer :parcc_protected_areas_id
+      t.integer :parcc_protected_area_id
       t.integer :parcc_chenge_type_id
       t.float :intersection_area
       t.float :overlap_percentage
--- db/schema.rb
+++ db/schema.rb
@@ -195,7 +195,7 @@ ActiveRecord::Schema.define(version: 20150424104630) do
 
   create_table "parcc_species_protected_areas", force: true do |t|
     t.integer  "parcc_species_id"
-    t.integer  "parcc_protected_areas_id"
+    t.integer  "parcc_protected_area_id"
     t.integer  "parcc_chenge_type_id"
     t.float    "intersection_area"
     t.float    "overlap_percentage"
