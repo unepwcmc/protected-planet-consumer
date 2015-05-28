d97278f7dfc8c221cda505bee1e2b97cb34c7d73
Miguel Torres <migtorres@gmail.com>
2015-05-28 11:35:14 +0100

    api tests passing with new search

--- app/models/gef/api/area.rb
+++ app/models/gef/api/area.rb
@@ -1,25 +1,53 @@
 class Gef::Api::Area < ActiveRecord::Base
-  def find_areas params: params
-    areas =  Gef::WdpaRecord.joins({gef_area: :gef_pame_records}, :gef_pame_name)
-    areas = areas.where(gef_areas: {gef_pmis_id: params[:gef_pmis_id]}) if params[:gef_pmis_id].present?
-    areas = areas.joins(gef_countries: :gef_region).where(gef_countries: {id: gef_country_id}) if gef_country_id.present?
-    areas = areas.joins(gef_countries: :gef_region).where(gef_countries: {gef_region_id: gef_region_id}) if gef_region_id.present?
-    areas = areas.where(gef_pame_records: {primary_biome: primary_biome}) if primary_biome.present?
-    areas = areas.where(wdpa_name: wdpa_name) if wdpa_name.present?
 
-    all_data = []
-    areas.each do |pa|
-      protected_area = pa.attributes.symbolize_keys!
-      protected_area.merge!( pa.gef_area.attributes.symbolize_keys! )
-      protected_area[:protected_planet_url] = protected_planet_url wdpa_id: protected_area[:wdpa_id]
-      protected_area[:wdpa_name] = protected_area[:wdpa_exists] ? protected_area[:wdpa_name] : pa.gef_pame_name.name
-      all_data << protected_area
-    end
-    all_data.uniq!
+  PP_BASE_URL = 'http://www.protectedplanet.net/sites/'
+
+  def self.area_finder params: params
+    @params = params
+    @areas ||= find_areas type: 'json'
+  end
+    
+  private
+
+  def self.find_areas type: type
+    areas = areas_querier
+    all_data = information areas: areas, type: type
     all_data
   end
 
-  def protected_planet_url wdpa_id: wdpa_id
+  def self.areas_querier
+    areas =  Gef::PameRecord.joins(:gef_area, :gef_pame_name, :gef_wdpa_records, :budget_recurrent_type, :budget_project_type)
+    areas = areas.where(gef_areas: {gef_pmis_id: @params[:gef_pmis_id]}) if @params[:gef_pmis_id].present?
+    areas = areas.joins(gef_wdpa_records: { gef_countries: :gef_region }).where(gef_countries: {id: @params[:gef_country_id]}) if @params[:gef_country_id].present?
+    areas = areas.joins(gef_wdpa_records: { gef_countries: :gef_region }).where(gef_countries: {gef_region_id: @params[:gef_region_id]}) if @params[:gef_region_id].present?
+    areas = areas.joins(:primary_biome).where(gef_biomes: { id: @params[:primary_biome_id]}) if @params[:primary_biome_id].present?
+    areas = areas.where(gef_wdpa_records: {wdpa_name: @params[:wdpa_name]}) if @params[:wdpa_name].present?
+    areas = areas.where(gef_wdpa_records: {wdpa_id: @params[:wdpa_id]}) if @params[:wdpa_id].present?
+    areas
+  end
+
+  def self.information areas: areas, type: type
+    @all_data = []
+    areas.each do |pa|
+      pa.gef_wdpa_records.each do |wdpa_area|
+        protected_area = pa.attributes.symbolize_keys!
+        protected_area.merge!( pa.gef_area.attributes.symbolize_keys! )
+        protected_area.merge!( pa.gef_pame_name.attributes.symbolize_keys! )
+        protected_area.merge!( wdpa_area.attributes.symbolize_keys! )
+        protected_area[:protected_planet_url] = protected_planet_url wdpa_id: protected_area[:wdpa_id]
+        protected_area[:wdpa_name] = protected_area[:wdpa_exists] ? protected_area[:wdpa_name] : pa.gef_pame_name.name
+        if type == 'page'
+          @all_data << protected_area.slice(:gef_pmis_id, :wdpa_id, :wdpa_name, :wdpa_exists, :protected_planet_url)
+        else
+          @all_data << protected_area
+        end
+      end
+    end
+    @all_data.uniq!
+    @all_data
+  end
+
+  def self.protected_planet_url wdpa_id: wdpa_id
     PP_BASE_URL + wdpa_id.to_s
   end
 
--- test/integration/gef/api_areas_test.rb
+++ test/integration/gef/api_areas_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class Gef::ApiAreasTest < ActionDispatch::IntegrationTest
-  test 'returns protected areas for a given gef_id' do
+
+  test 'returns protected areas for gef pmis id' do
 
     gef_area_1 = FactoryGirl.create(:gef_area, gef_pmis_id: 888999)
 
@@ -11,12 +12,55 @@ class Gef::ApiAreasTest < ActionDispatch::IntegrationTest
 
     gef_name_1 = FactoryGirl.create(:gef_pame_name, name: 'Manbone')
 
-    wdpa_record_1 = FactoryGirl.create(:gef_wdpa_record, wdpa_id: 999888)
+    wdpa_record_1 = FactoryGirl.create(:gef_wdpa_record, wdpa_id: 999888, legal_status: 'legal')
 
     gef_biome = FactoryGirl.create(:gef_biome, name: 'Manbone Biome')
 
+    gef_budget_type_1 = FactoryGirl.create(:gef_budget_type, name: 'Given')
+
+    gef_budget_type_2 = FactoryGirl.create(:gef_budget_type, name: 'Not Given')
+
     gef_pame_record_1 = FactoryGirl.create(:gef_pame_record, gef_area: gef_area_1, 
-                        gef_pame_name: gef_name_1, primary_biome_id: gef_biome.id, legal_status: 'legal')
+                        gef_pame_name: gef_name_1, primary_biome_id: gef_biome.id,
+                        budget_recurrent_type: gef_budget_type_1, budget_project_type: gef_budget_type_2)
+
+
+    FactoryGirl.create(:gef_pame_record_wdpa_record, gef_wdpa_record: wdpa_record_1,
+                        gef_pame_record: gef_pame_record_1)
+
+    get '/gef/api/areas?gef_pmis_id=888999'
+
+    assert response.success?
+
+    areas = JSON.parse(response.body, symbolize_names: true)
+
+    legal_status = areas[0][:legal_status]
+
+    assert_equal legal_status, 'legal'
+
+  end
+
+  test 'returns protected areas for all attributes' do
+
+    gef_area_1 = FactoryGirl.create(:gef_area, gef_pmis_id: 888999)
+
+    gef_region_1 = FactoryGirl.create(:gef_region, name: 'Manarctica')
+
+    gef_country_1 = FactoryGirl.create(:gef_country, name: 'Manboneland', gef_region: gef_region_1)
+
+    gef_name_1 = FactoryGirl.create(:gef_pame_name, name: 'Manbone')
+
+    wdpa_record_1 = FactoryGirl.create(:gef_wdpa_record, wdpa_id: 999888, legal_status: 'legal')
+
+    gef_biome = FactoryGirl.create(:gef_biome, name: 'Manbone Biome')
+
+    gef_budget_type_1 = FactoryGirl.create(:gef_budget_type, name: 'Given')
+
+    gef_budget_type_2 = FactoryGirl.create(:gef_budget_type, name: 'Not Given')
+
+    gef_pame_record_1 = FactoryGirl.create(:gef_pame_record, gef_area: gef_area_1,
+                        gef_pame_name: gef_name_1, primary_biome_id: gef_biome.id,
+                        budget_recurrent_type: gef_budget_type_1, budget_project_type: gef_budget_type_2)
 
 
     FactoryGirl.create(:gef_pame_record_wdpa_record, gef_wdpa_record: wdpa_record_1,
@@ -28,7 +72,7 @@ class Gef::ApiAreasTest < ActionDispatch::IntegrationTest
     
     areas = JSON.parse(response.body, symbolize_names: true)
 
-    legal_status = areas[0][:assessments][0][:budget_project_type]
+    legal_status = areas[0][:legal_status]
 
     assert_equal legal_status, 'legal'
 
