f071ddf4bc66990d8274229fe8f3c0c1e182505c
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2015-06-08 10:53:52 +0100

    Add missing relation for parcc_protected_area

--- app/models/parcc/protected_area.rb
+++ app/models/parcc/protected_area.rb
@@ -2,5 +2,6 @@ class Parcc::ProtectedArea < ActiveRecord::Base
   validates :parcc_id, uniqueness: true
 
   has_many :parcc_species_turnovers, class_name: 'Parcc::SpeciesTurnover', foreign_key: :parcc_protected_area_id
+  has_many :parcc_species_protected_areas, class_name: 'Parcc::SpeciesProtectedArea', foreign_key: :parcc_protected_area_id
   has_many :parcc_species, class_name: 'Parcc::Species', through: :parcc_species_protected_areas
 end
