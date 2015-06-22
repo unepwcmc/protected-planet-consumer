21af470f653b791556684e29ed07e2dd5e822c39
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2015-06-22 13:43:43 +0100

    Added title and map layout

--- app/helpers/parcc/protected_areas_helper.rb
+++ app/helpers/parcc/protected_areas_helper.rb
@@ -1,11 +1,13 @@
 module Parcc::ProtectedAreasHelper
+
+  PATH_TO_PROTECTED_AREA = -> wdpa_id { "http://www.protectedplanet.net/#{wdpa_id}" }
   def protected_planet_path page
     {
       root: 'http://www.protectedplanet.net',
       blog: 'http://blog.protectedplanet.net',
       about: 'http://www.protectedplanet.net/about',
-      un_list: 'http://blog.protectedplanet.net/post/102481051829/2014-united-nations-list-of-protected-areas'
+      un_list: 'http://blog.protectedplanet.net/post/102481051829/2014-united-nations-list-of-protected-areas',
+      protected_area: PATH_TO_PROTECTED_AREA
     }[page]
-
   end
 end
