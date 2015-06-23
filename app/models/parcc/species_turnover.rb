f227ec3f8092ffce981c2c8e60ee54fc0b0515c9
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-06-23 16:32:42 +0100

    add parcc_taxonomic_class relation to species_turnover

--- app/models/parcc/species_turnover.rb
+++ app/models/parcc/species_turnover.rb
@@ -1,3 +1,4 @@
 class Parcc::SpeciesTurnover < ActiveRecord::Base
   belongs_to :parcc_protected_area, class_name: 'Parcc::ProtectedArea', foreign_key: :parcc_protected_area_id
+  belongs_to :parcc_taxonomic_class, class_name: 'Parcc::TaxonomicClass', foreign_key: :taxonomic_class_id
 end
