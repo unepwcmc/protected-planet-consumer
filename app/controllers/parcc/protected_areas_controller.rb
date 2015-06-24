eb0cf025016b03e8a8d83b555df8cf66bb2793d0
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-06-24 18:27:39 +0100

    calculate grouped turnovers in ProtectedAreasController

--- app/controllers/parcc/protected_areas_controller.rb
+++ app/controllers/parcc/protected_areas_controller.rb
@@ -2,6 +2,19 @@ class Parcc::ProtectedAreasController < ApplicationController
   layout "parcc"
 
   def show
-    @protected_area = Parcc::ProtectedArea.find_by(wdpa_id: params[:id])
+    @protected_area = Parcc::ProtectedArea
+      .with_turnovers
+      .find_by(wdpa_id: params[:id])
+
+    @turnovers = grouped_turnovers(@protected_area.species_turnovers)
+  end
+
+  def grouped_turnovers all_turnovers
+    all_turnovers.each_with_object({}) { |turnover, groups|
+      class_name = turnover.taxonomic_class.name
+
+      groups[class_name] ||= {}
+      groups[class_name][turnover.year] = turnover
+    }
   end
 end
