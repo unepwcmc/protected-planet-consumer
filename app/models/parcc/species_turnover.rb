8d585bc491b4807eb023a3d118cd1a5dc832766b
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2015-07-06 14:58:43 +0100

    Fix percentage when value is 1

--- app/models/parcc/species_turnover.rb
+++ app/models/parcc/species_turnover.rb
@@ -3,6 +3,6 @@ class Parcc::SpeciesTurnover < ActiveRecord::Base
   belongs_to :taxonomic_class, class_name: 'Parcc::TaxonomicClass', foreign_key: :taxonomic_class_id
 
   def percentage(stat, round_to=1)
-    (send(stat)*100).round(round_to)
+    send(stat) >= 1 ? 100 : (send(stat)*100).round(round_to)
   end
 end
