048518f9e80cb8fd74a0be895a030c81a096837d
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-10-19 13:58:19 +0100

    merge import of taxonomic class counts into counts importer

--- lib/modules/parcc/importers/species/counts.rb
+++ lib/modules/parcc/importers/species/counts.rb
@@ -1,24 +1,39 @@
 class Parcc::Importers::Species::Counts < Parcc::Importers::Base
   def import
     csv_reader(source_file_path).each do |record|
-      protected_area = fetch_protected_area(record[:wdpa_id])
-      next unless protected_area
+      @protected_area = fetch_protected_area(record[:wdpa_id])
+      next unless @protected_area
 
-      add_counts_to_pa(protected_area, {
-        count_total_species: record[:count_total_species],
-        percentage_vulnerable_species: record[:percent_cc_vulnerable_species],
-        count_vulnerable_species: record[:count_cc_vulnerable_species]
-      })
+      @taxonomic_class = fetch_taxonomic_class(record[:taxon_class])
+      next unless @taxonomic_class
+
+      add_counts record
+      add_relation record
     end
   end
 
   private
 
-  def add_counts_to_pa protected_area, counts
-    protected_area.update_attributes(counts)
+  def add_relation record
+    Parcc::TaxonomicClassProtectedArea.create({
+      parcc_protected_area_id: @protected_area.id,
+      parcc_taxonomic_class_id: @taxonomic_class.id,
+      count_total_species: record[:total_species],
+      count_vulnerable_species: record[:vulnerable_species]
+    })
+  end
+
+  def add_counts record
+    @protected_area.count_total_species += record[:total_species]
+    @protected_area.count_vulnerable_species += record[:vulnerable_species]
+    @protected_area.percentage_vulnerable_species = (
+      @protected_area.count_vulnerable_species.to_f / @protected_area.count_total_species
+    ) * 100
+
+    @protected_area.save
   end
 
   def source_file_path
-    Rails.root.join('lib/data/parcc/species/wdpa_species_count.csv')
+    Rails.root.join('lib/data/parcc/species/taxonomic_class_protected_areas.csv')
   end
 end
--- test/unit/parcc/importers/species/counts_test.rb
+++ test/unit/parcc/importers/species/counts_test.rb
@@ -2,24 +2,40 @@ require 'test_helper'
 require 'csv'
 
 class TestParccImportersSpeciesCounts < ActiveSupport::TestCase
-  test '.import_counts adds counts to the found pa' do
-    pa = FactoryGirl.create(:parcc_protected_area, wdpa_id: 111222)
+  def setup
+    @pa = FactoryGirl.create(:parcc_protected_area, wdpa_id: 111222)
+    FactoryGirl.create(:parcc_taxonomic_class, name: 'Amphibian')
+    FactoryGirl.create(:parcc_taxonomic_class, name: 'Bird')
 
     csv = [{
       wdpa_id: 111222,
-      wdpa_name: 'abdoulaye',
-      count_total_species: 1000,
-      count_cc_vulnerable_species: 100,
-      percent_cc_vulnerable_species: 10
+      taxon_class: 'Amphibian',
+      vulnerable_species: 900,
+      non_vulnerable_species: 100,
+      total_species: 1000
+    }, {
+      wdpa_id: 111222,
+      taxon_class: 'Bird',
+      vulnerable_species: 10,
+      non_vulnerable_species: 20,
+      total_species: 30
     }]
 
     CSV.stubs(:foreach).returns(csv)
+  end
 
+  test '.import adds counts to the found pa' do
     Parcc::Importers::Species::Counts.import
 
-    pa = pa.reload
-    assert_equal 1000, pa.count_total_species
-    assert_equal 100, pa.count_vulnerable_species
-    assert_equal 10, pa.percentage_vulnerable_species
+    @pa = @pa.reload
+    assert_equal 1030, @pa.count_total_species
+    assert_equal 910, @pa.count_vulnerable_species
+    assert_equal 88, @pa.percentage_vulnerable_species
+  end
+
+  test '.import creates TaxonomicClassProtectedArea records' do
+    Parcc::Importers::Species::Counts.import
+    assert_equal 2, Parcc::TaxonomicClassProtectedArea.count
+    p Parcc::TaxonomicClassProtectedArea.all
   end
 end
