daf407be8daab48b7f8e38869e9ac0818953461a
Miguel Torres <migtorres@gmail.com>
2015-12-10 10:34:37 +0000

    removes parcc

--- app/models/parcc/species_protected_area.rb
+++
@@ -1,4 +0,0 @@
-class Parcc::SpeciesProtectedArea < ActiveRecord::Base
-  belongs_to :parcc_species, class_name: 'Parcc::Species', foreign_key: :parcc_species_id
-  belongs_to :parcc_protected_area, class_name: 'Parcc::ProtectedArea', foreign_key: :parcc_protected_area_id
-end
--- app/models/parcc/taxonomic_class.rb
+++
@@ -1,4 +0,0 @@
-class Parcc::TaxonomicClass < ActiveRecord::Base
-  validates :name, uniqueness: true
-  has_many :taxonomic_orders, class_name: 'Parcc::TaxonomicOrder', foreign_key: :parcc_taxonomic_class_id
-end
