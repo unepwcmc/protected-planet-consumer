b6ccbce9fd0e696d150a5995d8ea613b3827063a
Stuart Watson <stuart.watson@unep-wcmc.org>
2015-12-15 14:32:54 +0000

    Added production deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,21 @@
+set :stage, :production
+
+server "web-production.linode.unep-wcmc.org", user: 'wcmc', roles: %w{app web db}
+
+set :application, "ppe-reader"
+set :server_name, "ppe-reader.web-production.linode.unep-wcmc.org"
+set :sudo_user, "wcmc"
+set :app_port, "80"
+
+
+
+
+
+
+
+
+
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
